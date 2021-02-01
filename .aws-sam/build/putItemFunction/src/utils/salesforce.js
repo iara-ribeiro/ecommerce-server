e8107fe73fb22971280ad3ce1b3b3710e4b7f776
Iara Ribeiro <iara.ribeiro@altusgroup.com>
2021-02-01 18:10:23 -0500

    adding products code working

--- .aws-sam/build/putItemFunction/src/utils/salesforce.js
+++ .aws-sam/build/putItemFunction/src/utils/salesforce.js
@@ -67,87 +67,76 @@ async function readProducts(priceBookId, currencyCode, productId) {
     return await conn.apex.patch(url, body);
 }
 
+//https://developer.salesforce.com/docs/atlas.en-us.cpq_dev_api.meta/cpq_dev_api/cpq_api_quoteline_model_1.htm
+async function createQuoteItem(quote, product, productShopify) {
+    let quoteLineInfo = {
+        SBQQ__Quote__c: quote.Id,
+        SBQQ__Quantity__c: 1,
+        SBQQ__Product__c: product.record.PricebookEntries.records[0].Product2Id,
+        eComm_Unique_Id__c: quote.eComm_Unique_Id__c,
+        SBQQ__EndDate__c: quote.SBQQ__EndDate__c,
+        SBQQ__Quantity__c: productShopify.quantity,
+        SBQQ__ListPrice__c: productShopify.unitPrice,
+        SBQQ__Discount__c: productShopify.totalDiscount,
+        NS_ITO_ATTENDEE_EMAIL__c: productShopify.attendeeEmail,
+        NS_ITO_ATTENDEE_NAME__c: productShopify.attendeeName,
+        NS_ITO_ATTENDEE_PHONE__c: productShopify.attendeePhone,
+        Environment__c: productShopify.environmentId,
+        Product_Admin__c: null, //productShopify.adminId,
+        Selected_Session__c: productShopify.sessionId,
+        AVA_SFCPQ__TaxAmount__c: productShopify.taxAmount
+    }
+    
+    console.info('create quote line');
+    return await conn.sobject("SBQQ__QuoteLine__c").create([quoteLineInfo]);
+}
+
+async function updateQuoteLine(quote, product, productShopify) {
+    let quoteLine = await conn.sobject("SBQQ__QuoteLine__c")
+        .select('*')
+        .where(`SBQQ__Quote__c='${quote.Id}' and SBQQ__Product__c='${product.record.PricebookEntries.records[0].Product2Id}'`);
+
+    console.info(`update quote line for ${quote.Id}, ${product.record.PricebookEntries.records[0].Product2Id}`);
+    let newQuoteLineInfo = {
+        eComm_Unique_Id__c: quote.eComm_Unique_Id__c,
+        SBQQ__EndDate__c: quote.SBQQ__EndDate__c,
+        SBQQ__Quantity__c: productShopify.quantity,
+        SBQQ__ListPrice__c: productShopify.unitPrice,
+        SBQQ__Discount__c: productShopify.totalDiscount,
+        NS_ITO_ATTENDEE_EMAIL__c: productShopify.attendeeEmail,
+        NS_ITO_ATTENDEE_NAME__c: productShopify.attendeeName,
+        NS_ITO_ATTENDEE_PHONE__c: productShopify.attendeePhone,
+        Environment__c: productShopify.environmentId,
+        Product_Admin__c: productShopify.adminId,
+        Selected_Session__c: productShopify.sessionId,
+        AVA_SFCPQ__TaxAmount__c: productShopify.taxAmount,
+        AVA_SFCPQ__SalesTaxDetails__c: productShopify.taxDetails
+    };
+
+    let updatedQuote = await conn.sobject("SBQQ__QuoteLine__c")
+        .find({ Id : quoteLine[0].Id })
+        .update(newQuoteLineInfo);
+    
+    return updatedQuote;
+}
+
 /** Map the products from shopify to the quote line
  * https://developer.salesforce.com/docs/atlas.en-us.cpq_dev_api.meta/cpq_dev_api/cpq_api_product_model_8.htm
  */
 async function addQuoteProducts(lineItems, quote) {
     console.info(`start adding the products to quote - ${quote.Id}`);
-
     let result;
-    const groupKey = 0;
-    const ignoreCalculate = true;
-    const url = '/SBQQ/ServiceRouter?loader=SBQQ.QuoteAPI.QuoteProductAdder';
 
     if (lineItems.length > 0) {
         result = await Promise.all(lineItems.map( async (item) => {
             let product = JSON.parse(await readProducts('01s2M000008d5y8QAA', quote.CurrencyIsoCode, item.productId));
-            
-            //Pass the product to the product model format
-            /* Id configuredProductId; 
-               Id optionId; 
-               SBQQ__ProductOption__c optionData; // Editable data about the option in question, such as quantity or discount 
-               SBQQ__ProductOption__c configurationData; 
-               SBQQ__ProductOption__c inheritedConfigurationData; 
-               ConfigurationModel[] optionConfigurations; 
-               Boolean configured; 
-               Boolean changedByProductActions; 
-               Boolean isDynamicOption; 
-               Boolean isUpgrade; 
-               Set<Id> disabledOptionIds; 
-               Set<Id> hiddenOptionIds; 
-               Decimal listPrice; 
-               Boolean priceEditable; 
-               String[] validationMessages; 
-               String dynamicOptionKey; */
-            
-            /*let productConfiguration = {
-                configuredProductId: produc.Id,
-                optionId: item.Id,
-                configured: false,
-                changedByProductActions: false,
-                isDynamicOption: true,
-                isUpgrade: false,
-                priceEditable: false
-            };*/
-            
-            /*Product2 record;
-              Id upgradedAssetId;
-              String currencySymbol;
-              String currencyCode;
-              String[] featureCategories;
-              OptionModel[] options;
-              FeatureModel[] features;
-              ConfigurationModel configuration;
-              ConfigAttributeModel[] configurationAttributes;
-              ConfigAttributeModel[] inheritedConfigurationAttributes;
-              ConstraintModel[] constraints; 
-            */
-
-            /*let productModel = {
-                record: product.record,
-                currencySymbol: product.currencySymbol,
-                currencyCode: product.currencyCode,
-                featureCategories: product.featureCategories,
-                options: product.options,
-                features: product.features,
-                //configuration: productConfiguration,
-                configurationAttributes: product.configurationAttributes,
-                inheritedConfigurationAttributes: [],
-                constraints: product.constraints
-            }
-
-            //console.info(productModel);
-            console.info(product);*/
-            return product;
-
+            let lineItem = createQuoteItem(quote, product, item);
+            //let lineItem = updateQuoteLine(quote, product, item);
+            return lineItem;
          }));     
     }
 
-    //var context = { quote: quote, groupKey: groupKey, products: products, ignoreCalculate: ignoreCalculate };
-    const args = { quote: { record:  quote }, products: result, groupKey: groupKey, ignoreCalculate: ignoreCalculate };
-    const body = { "context": JSON.stringify(args) };
-
-    return await conn.apex.patch(url, body);
+    return result;
 }
 
 /** Calculates the prices of a CPQ quote created at SF */
@@ -224,8 +213,8 @@ async function createQuote(transaction) {
                 Approval_Type__c: 'e-mail / Attach',
                 ApprovalStatus__c: 'Approved',
                 SBQQ__PaymentTerms__c: 'Due on receipt',
-                SBQQ__PriceBook__c: priceBook.Id,
-                SBQQ__PriceBookId__c: priceBook.Id,
+                SBQQ__PriceBook__c: "01s2M000008d5y8QAA",
+                SBQQ__PriceBookId__c: "01s2M000008d5y8QAA",
                 Ship_to_Contact__c: salesForce.contactId,
                 Bill_to_Contact__c: salesForce.contactId,
                 Promo_Code__c: promoCode
@@ -246,14 +235,25 @@ async function createQuote(transaction) {
             }*/
         }
 
-        return await addQuoteProducts(transaction.lineItems, quoteSF[0]);
+        let lineItems = await addQuoteProducts(transaction.lineItems, quoteSF[0]);
 
-        /*let quoteModel = JSON.parse(await calculateQuote(quoteSF));
+        return lineItems;
+        //return quoteSF;
+        /*console.info(lineItems);
 
-        return await validateQuote(quoteModel.record);
+        let updatedQuote = await conn.sobject('SBQQ__Quote__c').update(
+            { 
+                Id : quoteSF[0].Id,
+                lineItems : lineItems
+            }
+        );
 
-        //return saveQuote(quoteSF);
-        */
+        return updatedQuote;*/
+        //let quoteModel = JSON.parse(await calculateQuote(quoteSF[0]));
+        //return await validateQuote(quoteModel.record);
+
+        //return saveQuote(quoteSF[0]);
+        
     } catch (err) {
         console.info(err);
         throw (err);
